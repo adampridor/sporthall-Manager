fd2e7d5a440d55e6c0ad71a2c292c8b8e56dec4b
adampridor <adam@pridor.com>
2024-10-07 13:39:26 +0300

    first version

---
+++ app/src/main/java/com/example/sporthallmanager/MainCalendarActivity.java
@@ -0,0 +1,38 @@
+package com.example.sporthallmanager;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+public class MainCalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_calendar);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+        });
+        CalendarView calendarView = findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                // Note that months are indexed from 0
+                String date = dayOfMonth + "/" + (month + 1) + "/" + year;
+                Toast.makeText(getApplicationContext(), date, Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+}
