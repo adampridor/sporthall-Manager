78acef8be0e65fbbb057e7ab8f56e7477876ba9b
adampridor <adam@pridor.com>
2024-11-05 09:57:01 +0200

    4;30 video

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation ("com.github.bumptech.glide:glide:4.16.0'")
-    implementation ("com.google.android.gms:playservices-auth:20.7.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0'")
+    implementation("com.google.android.gms:playservices-auth:20.7.0")
 }
