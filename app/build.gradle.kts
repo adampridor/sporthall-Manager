f19690d0f164b3ad466a425ef0a1fda6542d8915
adampridor <adam@pridor.com>
2025-01-21 10:57:00 +0200

    no errors but compilation error circuler dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation ("androidx.recyclerview:recyclerview:1.3.2")
+    implementation ("androidx.exifinterface:exifinterface:1.3.6")
 }
--- app/src/main/java/com/example/sporthallmanager/HourlyScheduleActivity.java
+++ app/src/main/java/com/example/sporthallmanager/HourlyScheduleActivity.java
@@ -11,6 +11,7 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
